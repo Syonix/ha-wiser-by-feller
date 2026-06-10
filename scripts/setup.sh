36f0193fd69efa9d28e9f7e978b5298267c48c14
Michael Burri <michael.burri@syonix.ch>
2026-06-10 09:00:09 +0200

    Migrate dev container to recommended docker setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,12 +6,7 @@ cd "$(dirname "$0")/.."
 
 alias lint='./scripts/lint.sh'
 
-# Create config dir if not present
-if [[ ! -d "${PWD}/config" ]]; then
-    mkdir -p "${PWD}/config"
-    hass --config "${PWD}/config" --script ensure_config
-fi
-
 # Install dependencies
 python3 -m pip install --requirement requirements.txt
-python3 -m pip install --requirement devcontainer/requirements.txt
+python3 -m pip install --requirement requirements.dev.txt
+python3 -m pip install --requirement requirements.test.txt
